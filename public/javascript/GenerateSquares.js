86a11f66aaf90d9b97c577953c25d65857a00f0a
Oscar Nordquist <on222bx@student.lnu.se>
2016-09-24 21:50:48 +0200

    Generating squares

--- public/javascript/GenerateSquares.js
+++ public/javascript/GenerateSquares.js
@@ -1,9 +1,36 @@
-"use script";
+'use strict'
 
-class GenerateSquares {
-  constructor() {
-    this.container = document.querySelector('.container');
+class SquareGenerator {
+
+  constructor () {
+    this.container = document.querySelector('.container')
+    this.existingSquares = []
   }
+
+  insertSquareIntoArray (square) {
+    this.existingSquares.push(square)
+  }
+
+  generateSquare () {
+    let square = document.createElement('div')
+    square.classList.add('square')
+    return square
+  }
+
+  appendSquares () {
+    for (let square of this.existingSquares) {
+      this.container.appendChild(square)
+    }
+  }
+
+  insertMultipleSquares (amount) {
+    for (let i = 0; i < amount; i++) {
+      this.insertSquareIntoArray(this.generateSquare())
+    }
+  }
+
 }
 
-const gs = new GenerateSquares();
+let sg = new SquareGenerator()
+sg.insertMultipleSquares(10)
+sg.appendSquares()
