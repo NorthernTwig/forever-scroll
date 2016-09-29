5c002321ffdb22800023c240fee6cbd68b31eff0
Oscar Nordquist <on222bx@student.lnu.se>
2016-09-29 21:49:37 +0200

    Generates the right amount of squares

--- public/javascript/GenerateSquares.js
+++ public/javascript/GenerateSquares.js
@@ -1,36 +1,120 @@
 'use strict'
 
-class SquareGenerator {
+class GenerateSquare {
 
-  constructor () {
-    this.container = document.querySelector('.container')
-    this.existingSquares = []
-  }
-
-  insertSquareIntoArray (square) {
-    this.existingSquares.push(square)
-  }
-
-  generateSquare () {
-    let square = document.createElement('div')
-    square.classList.add('square')
-    return square
-  }
-
-  appendSquares () {
-    for (let square of this.existingSquares) {
-      this.container.appendChild(square)
+    constructor() {
+        this.container = document.querySelector('.container')
     }
+
+    appendRowToContainer(row) {
+        this.container.appendChild(row)
+    }
+
+    appendSquareToRow(row, square) {
+        row.appendChild(square)
+    }
+
+    create(element) {
+        return document.createElement(element)
+    }
+
+    generateSquare() {
+        let square = this.create('div')
+        square.classList.add('square')
+        return square
+    }
+
+    generateRow() {
+        let row = this.create('div')
+        row.classList.add('row')
+        return row
+    }
+
+    generateAll(rows, squares) {
+        for(let i = 0; i < rows; i++) {
+            let row = this.generateRow()
+            for(let j = 0; j < squares; j++) {
+                let square = this.generateSquare()
+                this.appendSquareToRow(row, square)
+            }
+            this.appendRowToContainer(row)
+        }
+    }
+}
+
+class ScrollChecker {
+
+    constructor() {
+        this.container = document.querySelector('.container')
+        this.gS = new GenerateSquare()
+        this.x = 1;
+        this.y = 5;
+    }
+
+    initialize() {
+        this.appendCheckerToContainer(this.generateCheckElement())
+    }
+
+    generateCheckElement() {
+        let checker = document.createElement('div')
+        checker.classList.add('checker')
+        checker.addEventListener('click', this.generateMoreSquares.bind(this))
+        return checker
+    }
+
+    countSquares() {
+        let count = 0;
+        for(let i = 0; i < this.container.children.length; i++) {
+            count += this.container.children[i].children.length
+        }
+        return count;
+    }
+
+    generateMoreSquares() {
+        if(this.countSquares() < this.setImageLimit()) {
+            if(this.setImageLimit() - this.countSquares() > 5) {
+                this.gS.generateAll(this.x, this.y)
+            } else {
+                this.y = this.setImageLimit() - this.countSquares()
+                this.gS.generateAll(this.x, this.y)
+            }
+        }
+        this.countSquares()
+    }
+
+    appendCheckerToContainer(checker) {
+        this.container.appendChild(checker)
+    }
+
+    setImageLimit() {
+        return 37;
+    }
+
+}
+
+class ImageCollection {
+  constructor() {
+    this.getImage()
   }
 
-  insertMultipleSquares (amount) {
-    for (let i = 0; i < amount; i++) {
-      this.insertSquareIntoArray(this.generateSquare())
-    }
+  pushSrcIntoArray() {
+    
+  }
+
+  getImage() {
+    fetch('https://unsplash.it/500/500/?random')
+    .then(e => e.blob())
+    .then(blob => URL.createObjectURL(blob))
+    .then(final => final)
+    .catch(error => console.log(error))
   }
 
 }
 
-let sg = new SquareGenerator()
-sg.insertMultipleSquares(10)
-sg.appendSquares()
+/**
+ * @param {int} Rows
+ * @param {int} Squares per Row
+ */
+const sC = new ScrollChecker()
+const iC = new ImageCollection()
+sC.initialize()
